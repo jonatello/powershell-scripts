7b5b5961fcbb4b149c0063911a8021f4c1439975
jonatello <ashevillainy@gmail.com>
2018-11-13 11:10:33 -0500

    Update Get-MicrosoftLicensing.ps1
    Modified to mark N/A when null. Replaced pscustomobject with PSObject method for PSv2.0 compatibility.

--- Get-MicrosoftLicensing.ps1
+++ Get-MicrosoftLicensing.ps1
@@ -2,6 +2,25 @@ Function Get-MicrosoftLicensing {
     # Get all SoftwareLicensingService objects via WMI
     Try {
         $SoftwareLicensingService = Get-WMIObject -Class SoftwareLicensingService
+
+        If ($($SoftwareLicensingService.Version) -notlike $null) {
+            $Version = $($SoftwareLicensingService.Version)
+        } Else {
+            $Version = 'N/A'
+        }
+
+        If ($($SoftwareLicensingService.OA3xOriginalProductKeyDescription) -notlike $null) {
+            $OA3xOriginalProductKeyDescription = $($SoftwareLicensingService.OA3xOriginalProductKeyDescription)
+        } Else {
+            $OA3xOriginalProductKeyDescription = 'N/A'
+        }
+
+        If ($($SoftwareLicensingService.OA3xOriginalProductKey) -notlike $null) {
+            $OA3xOriginalProductKey = $($SoftwareLicensingService.OA3xOriginalProductKey)
+        } Else {
+            $OA3xOriginalProductKey = 'N/A'
+        }
+
     } Catch {
         Write-Error "There was an exception querying SoftwareLicensingService: $_"
     }
@@ -9,6 +28,43 @@ Function Get-MicrosoftLicensing {
     # Get all SoftwareLicensingProduct objects via WMI
     Try {
         $SoftwareLicensingProduct = Get-WMIObject -Class SoftwareLicensingProduct | Where-Object {($_.LicenseStatus -ne "0") -and ($_.ApplicationID -ne "55c92734-d682-4d71-983e-d6ec3f16059f") -and ($_.Name -like "*Office*")}
+
+        If ($($SoftwareLicensingProduct.Count) -notlike $null) {
+            $Count = $($SoftwareLicensingProduct.Count)
+        } Else {
+            $Count = 'N/A'
+        }
+
+        If ($($SoftwareLicensingProduct.Name) -notlike $null) {
+            $Name = $($SoftwareLicensingProduct.Name) | Out-String
+        } Else {
+            $Name = 'N/A'
+        }
+
+        If ($($SoftwareLicensingProduct.ProductKeyChannel) -notlike $null) {
+            $ProductKeyChannel = $($SoftwareLicensingProduct.ProductKeyChannel) | Out-String
+        } Else {
+            $ProductKeyChannel = 'N/A'
+        }
+
+        If ($($SoftwareLicensingProduct.LicenseName) -notlike $null) {
+            $LicenseName = $($SoftwareLicensingProduct.LicenseName) | Out-String
+        } Else {
+            $LicenseName = 'N/A'
+        }
+
+        If ($($SoftwareLicensingProduct.LicenseDescription) -notlike $null) {
+            $LicenseDescription = $($SoftwareLicensingProduct.LicenseDescription) | Out-String
+        } Else {
+            $LicenseDescription = 'N/A'
+        }
+
+        If ($($SoftwareLicensingProduct.LicenseStatus) -notlike $null) {
+            $LicenseStatus = $($SoftwareLicensingProduct.LicenseStatus) | Out-String
+        } Else {
+            $LicenseStatus = 'N/A'
+        }
+
     } Catch {
         Write-Error "There was an exception while querying SoftwareLicensingProduct: $_"
     }
@@ -24,24 +80,26 @@ Function Get-MicrosoftLicensing {
             $LicenseDescription = (($OSPP | Select-String -Pattern 'LICENSE DESCRIPTION: ') -replace 'License Description: ','')
             $LicenseStatus = (($OSPP | Select-String -Pattern 'LICENSE STATUS: ') -replace 'License Status: ','')
         } Else {
-            $LicenseName = "N/A"
-            $LicenseDescription = "N/A"
-            $LicenseStatus = "N/A"
+            $LicenseName = 'N/A'
+            $LicenseDescription = 'N/A'
+            $LicenseStatus = 'N/A'
         }
+
     } Catch {
         Write-Error "There was an exception while running ospp.vbs: $_"
     }
 
     # Create a custom object to hold the results
-    [PSCustomObject]@{
-        'Operating System Version (WMI)' = $($SoftwareLicensingService.Version)
-        'Windows Edition (WMI)' = $($SoftwareLicensingService.OA3xOriginalProductKeyDescription)
-        'Windows Product Key (WMI)' = $($SoftwareLicensingService.OA3xOriginalProductKey)
-        'Office Product Count (WMI)' = $($SoftwareLicensingProduct.Count)
-        'Office Product Name (WMI)' = ($($SoftwareLicensingProduct.Name) | Out-String)
-        'Office Product Channel (WMI)' = ($($SoftwareLicensingProduct.ProductKeyChannel) | Out-String)
-        'Office License Name (OSPP)' = ($LicenseName | Out-String)
-        'Office License Description (OSPP)' = ($LicenseDescription | Out-String)
-        'Office License Status (OSPP)' = ($LicenseStatus | Out-String)
+    New-Object -TypeName PSObject -Property @{
+        'Operating System Version (WMI)' = $Version
+        'Windows Edition (WMI)' = $OA3xOriginalProductKeyDescription
+        'Windows Product Key (WMI)' = $OA3xOriginalProductKey
+        'Office Product Count (WMI)' = $Count
+        'Office Product Name (WMI)' = $Name
+        'Office Product Channel (WMI)' = $ProductKeyChannel
+        'Office License Name (OSPP)' = $LicenseName
+        'Office License Description (OSPP)' = $LicenseDescription
+        'Office License Status (OSPP)' = $LicenseStatus
+        'License Audit Performed' = (Get-Date -Format g)
     } | Write-Output
 }
