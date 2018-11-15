5ccfc152a6bc3261fd71fc6217ed58f722f4a17f
jonatello <ashevillainy@gmail.com>
2018-11-15 16:34:20 -0500

    Create Get-ExternalIP.ps1

---
+++ Get-ExternalIP.ps1
@@ -0,0 +1,34 @@
+Function Get-ExternalIP {
+    <#
+    .SYNOPSIS
+    Get External IP via ipinfo.io
+    If no IP is specified, it will check your public IP
+    Author: Jon Rodriguez
+    .DESCRIPTION
+    
+    .NOTES
+    Requires PowerShell 3.0+
+    .EXAMPLE
+    Get-ExternalIP -IP 8.8.8.8
+    #>
+    
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false,Position = 0)]
+        [string]$IP
+    )
+
+    If ($IP = $null) {
+        Try {
+            Invoke-RestMethod https://ipinfo.io/json
+        } Catch {
+            Write-Error "There was an issue querying ipinfo.io:`n`n$_"
+        }
+    } Else {
+        Try {
+            Invoke-RestMethod https://ipinfo.io/$IP
+        } Catch {
+            Write-Error "There was an issue querying ipinfo.io:`n`n$_"
+        }
+    }
+}
